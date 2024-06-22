e55f2a901dad42699dece69b04ecc4d6a1c8b1b8
jocha college <gregorykimanthi100@gmail.com>
2024-06-22 23:49:16 +0300

    add date validation

--- public/script/validation.js
+++ public/script/validation.js
@@ -4,103 +4,97 @@ const year = document.querySelector("#year");
 const submit = document.querySelector("#submit");
 const errorMessage = document.querySelector("form p");
 
-const invalidDateError = document.querySelector("#invalidDate-error")
-const invalidMonthError = document.querySelector("#invalidMonth-error")
-const invalidYearError = document.querySelector("#invalidYear-error")
+const invalidDateError = document.querySelector("#invalidDate-error");
+const invalidMonthError = document.querySelector("#invalidMonth-error");
+const invalidYearError = document.querySelector("#invalidYear-error");
 
 const inputField = document.querySelectorAll(".input-container input");
 const submitButton = document.querySelector("#submit");
 const form = document.querySelector("form");
 
+const showError = function (input) {
+  var parentDiv = input.closest(".input-container");
+  var error = parentDiv.querySelector("p");
+  error.classList.add("show-error");
+  parentDiv.querySelector("label").classList.add("change-color");
+  parentDiv.querySelector("input[type='text']").classList.add("show-outline");
+};
 
-const showError = function(input){
-    var parentDiv = input.closest(".input-container")
-    var error = parentDiv.querySelector("p");
-    error.classList.add("show-error");
-    parentDiv.querySelector("label").classList.add("change-color");
-    parentDiv.querySelector("input[type='text']").classList.add("show-outline");
-
-}
-
-const removeError = function(input){
-    var parentDiv = input.closest(".input-container")
-    var error = parentDiv.querySelector("p");
-    error.classList.remove("show-error");
-    parentDiv.querySelector("label").classList.remove("change-color");
-    parentDiv.querySelector("input[type='text']").classList.remove("show-outline");
-
-}
-inputField.forEach((input,currentIndex) => {
-    input.addEventListener("input",()=>{
-        const checkInput = input.value.trim() ==="";
-        console.log(currentIndex);
-        if (checkInput){
-            showError(input);    
-        }
-        else{
-           removeError(input);
-        }
-    });
+const removeError = function (input) {
+  var parentDiv = input.closest(".input-container");
+  var error = parentDiv.querySelector("p");
+  error.classList.remove("show-error");
+  parentDiv.querySelector("label").classList.remove("change-color");
+  parentDiv
+    .querySelector("input[type='text']")
+    .classList.remove("show-outline");
+};
+inputField.forEach((input, currentIndex) => {
+  input.addEventListener("input", () => {
+    const checkInput = input.value.trim() === "";
+    console.log(currentIndex);
+    if (checkInput) {
+      showError(input);
+    } else {
+      removeError(input);
+    }
+  });
 });
 
-submit.addEventListener("click",function(event){
-   event.preventDefault(); 
-   
-   inputField.forEach((input,currentIndex) => {
-    const emptyInput = Array.from(inputField).some((input) =>input.value.trim() === "")
-    console.log(currentIndex)
-     
-    if(emptyInput){
-        // alert("please input a value");
-        showError(input);
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
 
-    }
-    else{
-        form.submit();
-        // alert("submiting values...");
-    }
-   });
-})
+  inputField.forEach((input, currentIndex) => {
+    const emptyInput = Array.from(inputField).some(
+      (input) => input.value.trim() === ""
+    );
+    console.log(currentIndex);
 
-day.addEventListener("input", function(){
+    if (emptyInput) {
+      // alert("please input a value");
+      showError(input);
+    } else {
+      form.submit();
+      // alert("submiting values...");
+    }
+  });
+});
+
+inputField.forEach((input, currentIndex) => {
+  input.addEventListener("input", () => {
     const inputDate = parseInt(day.value);
-    console.log(typeof inputDate)
-    if(inputDate > 31){
-        invalidDateError.classList.add("show-error")
-        
-    }
-    else{
-        invalidDateError.classList.remove("show-error") 
+    console.log(inputDate);
+  });
+});
 
-    } 
+day.addEventListener("input", function () {
+  const inputDate = parseInt(day.value);
+  console.log(typeof inputDate);
+  if (inputDate > 31) {
+    invalidDateError.classList.add("show-error");
+  } else {
+    invalidDateError.classList.remove("show-error");
+  }
+});
 
-})
+month.addEventListener("input", function () {
+  const inputDate = parseInt(month.value);
+  console.log(typeof inputDate);
+  if (inputDate > 12) {
+    invalidMonthError.classList.add("show-error");
+  } else {
+    invalidMonthError.classList.remove("show-error");
+  }
+});
 
-
-month.addEventListener("input", function(){
-    const inputDate = parseInt(month.value);
-    console.log(typeof inputDate)
-    if(inputDate > 12){
-        invalidMonthError.classList.add("show-error")
-        
-    }
-    else{
-        invalidMonthError.classList.remove("show-error") 
-
-    } 
-
-})
-
-year.addEventListener("input", function(){
-    const inputDate = parseInt(year.value);
-    console.log(typeof inputDate)
-    if(inputDate > 12){
-        invalidYearError.classList.add("show-error")
-        
-    }
-    else{
-        invalidYearError.classList.remove("show-error") 
-
-    } 
-
-})
+year.addEventListener("input", function () {
+  const inputDate = parseInt(year.value);
+  console.log(typeof inputDate);
+  if (inputDate > 12) {
+    invalidYearError.classList.add("show-error");
+    // showError();
+  } else {
+    invalidYearError.classList.remove("show-error");
+    // removeError();
+  }
+});
