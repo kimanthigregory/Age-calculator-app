2e0df7750f5cae4f112efeaff0cc2572490edeea
JOCHA COLLEGE <gregorykimanthi100@gmail.com>
2024-06-27 18:34:45 +0300

    add input validatiion

--- public/script/validation.js
+++ public/script/validation.js
@@ -24,7 +24,7 @@ const removeErrortwo =function(input){
 }
 const showError = function (input) {
   var parentDiv = input.closest(".input-container");
-  var error = parentDiv.querySelector("p");
+  // var error = parentDiv.querySelector("p");
   parentDiv.querySelector("label").classList.add("change-color");
   parentDiv.querySelector("input[type='text']").classList.add("show-outline");
 };
@@ -32,13 +32,15 @@ const showError = function (input) {
 const removeError = function (input) {
   var parentDiv = input.closest(".input-container");
   parentDiv.querySelector("label").classList.remove("change-color");
-  parentDiv.querySelector("input[type='text']")
-    parentDiv.classList.remove("show-outline");
+  parentDiv.querySelector("input[type='text']").classList.remove("show-outline");
 };
 inputField.forEach((input, currentIndex) => {
   input.addEventListener("input", () => {
     const checkInput = input.value.trim() === "";
     // console.log(currentIndex);
+    const parentDiv = input.closest(".input-container");
+      
+    const inputName = input.getAttribute('id');
     if (checkInput) {
         showError(input);
         showErrorTwo(input);
@@ -46,82 +48,94 @@ inputField.forEach((input, currentIndex) => {
         removeError(input);
         removeErrortwo(input);
     }
+    if (inputName === 'day'){
+      validateDate(input,parentDiv)
+    } else if(inputName === 'month') {
+      validateMonth(input, parentDiv);
+    } else if (inputName === 'year'){
+      validateYear(input,parentDiv);
+    }
+   
   });
 });
 
+let validated = true;
+
 submit.addEventListener("click", function (event) {
   event.preventDefault();
-
+  validated = true;
   inputField.forEach((input, currentIndex) => {
     const emptyInput = Array.from(inputField).some(
-      (input) => input.value.trim() === ""
+      (input) => input.value.trim() === "" 
     );
+    const parentDiv = input.closest(".input-container");
+      
+    const inputName = input.getAttribute('id');
+    console.log(inputName);
     // console.log(currentIndex);
-
+    
     if (emptyInput) {
       // alert("please input a value");
       showError(input);
-    } else {
+      validated = false;
+    }  else{
+      if (inputName === 'day'){
+        validateDate(input,parentDiv)
+      } else if(inputName === 'month') {
+        validateMonth(input, parentDiv);
+      } else if (inputName === 'year'){
+        validateYear(input,parentDiv);
+      }
+     
+    }
+    if (validated){
       form.submit();
       // alert("submiting values...");
     }
   });
 });
 
-inputField.forEach((input, currentIndex) => {
-  input.addEventListener("input", () => {
-    const inputDate = parseInt(input.value);
-    const parentDiv = input.closest(".input-container");
-    console.log(currentIndex)
-    console.log(inputDate);
-    if (inputDate > 31) {
-        parentDiv.querySelector("#invalid-error").classList.add("show-error");
-        showError(input);
-    } else if (inputDate > 12) {
-        parentDiv.querySelector("#invalid-error").classList.add("show-error");
-        showError(input);
-    } else if (inputDate > 2024) {
-        console.log(input);
-        parentDiv.querySelector("#invalid-error").classList.add("show-error");
-        showError(input);
-    } else {
-        parentDiv.querySelector("#invalid-error").classList.remove("show-error");
-        removeError(input);
-    }
-  });
+
+
+const validateDate = function(input,parentDiv){
+  const inputValue = parseInt(input.value);
+  if (inputValue > 31) {
+    parentDiv.querySelector("#invalid-error").classList.add("show-error");
+    showError(input);
+    validated = false;
+  } else{
+    parentDiv.querySelector("#invalid-error").classList.remove("show-error");
+    removeError(input);
     
+  }
+}
+
+
+const validateMonth = function (input,parentDiv){
+  const inputValue = parseInt(input.value);
+  if (inputValue > 12) {
+  console.log(input);
+  parentDiv.querySelector("#invalid-error").classList.add("show-error");
+  showError(input);
+  validated = false;
+}else{
+  parentDiv.querySelector("#invalid-error").classList.remove("show-error");
+  removeError(input);
+
+}
+}
+
+const validateYear = function(input,parentDiv){
+  const inputValue = parseInt(input.value);
+  console.log(inputValue);
+  if (inputValue > 2024) {
+    console.log(input);
+    parentDiv.querySelector("#invalid-error").classList.add("show-error");
+    showError(input);
+    validated = false;
+  }else{
+    parentDiv.querySelector("#invalid-error").classList.remove("show-error");
+    removeError(input);
+  }
   
-});
-
-
-// day.addEventListener("input", function () {
-//   const inputDate = parseInt(day.value);
-//   console.log(typeof inputDate);
-//   if (inputDate > 31) {
-//     invalidDateError.classList.add("show-error");
-//   } else {
-//     invalidDateError.classList.remove("show-error");
-//   }
-// });
-
-// month.addEventListener("input", function () {
-//   const inputDate = parseInt(month.value);
-//   console.log(typeof inputDate);
-//   if (inputDate > 12) {
-//     invalidMonthError.classList.add("show-error");
-//   } else {
-//     invalidMonthError.classList.remove("show-error");
-//   }
-// });
-
-// year.addEventListener("input", function () {
-//   const inputDate = parseInt(year.value);
-//   console.log(typeof inputDate);
-//   if (inputDate > 12) {
-//     invalidYearError.classList.add("show-error");
-//     // showError();
-//   } else {
-//     invalidYearError.classList.remove("show-error");
-//     // removeError();
-//   }
-// });
+}
