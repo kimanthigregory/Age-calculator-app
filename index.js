b326c2d0e22de8bfd388aed76aba9ff29120d38d
jocha college <gregorykimanthi100@gmail.com>
2024-07-02 23:03:24 +0300

    work on age calculation

--- index.js
+++ index.js
@@ -6,54 +6,63 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/" , (req,res)=>{
-    
-    res.render(
-        "index.ejs",{
-            date:null,
-            month:null,
-            year:null
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    date: null,
+    month: null,
+    year: null,
+  });
+});
 
-        })
-   
-})
+app.post("/input", (req, res) => {
+  const usersDate = parseInt(req.body.date);
+  const usersMonth = parseInt(req.body.month) - 1;
+  const usersYear = parseInt(req.body.year);
+  const ageCalculator = function (year, month, date) {
+    const createDate = new Date(year, month, date);
+    const today = new Date();
 
-app.post("/input",(req,res)=>{
-    const usersDate =parseInt(req.body.date);
-    const usersMonth =parseInt(req.body.month)-1;
-    const usersYear = parseInt(req.body.year);
-    const ageCalculator = function(year,month,date){
-        const createDate = new Date(year,month,date)
-        const today = new Date();
-
-        const dateOfBirth = today.getDate() - createDate.getDate();
-        const monthOfBirth = today.getMonth() - createDate.getMonth();
-        let yearOfBirth = today.getFullYear() - createDate.getFullYear();
-        if (monthOfBirth <0 || usersMonth === createDate.getMonth() && dateOfBirth < 0){
-            yearOfBirth--
-        }
-
-        return {dateOfBirth, monthOfBirth, yearOfBirth};
-    
+    let dateOfBirth = today.getDate() - createDate.getDate();
+    let monthOfBirth = today.getMonth() - createDate.getMonth();
+    let yearOfBirth = today.getFullYear() - createDate.getFullYear();
+    if (
+      monthOfBirth < 0 ||
+      (usersMonth === createDate.getMonth() && dateOfBirth < 0)
+    ) {
+      yearOfBirth -= 1;
+      monthOfBirth += 12;
     }
-    console.log(ageCalculator(usersYear,usersMonth,usersDate));
-    let {dateOfBirth, monthOfBirth, yearOfBirth} = ageCalculator(usersYear,usersMonth,usersDate);
-  
-    res.render(
-        "index.ejs",{
-            date:Math.abs(dateOfBirth),
-            month:Math.abs(monthOfBirth),
-            year:Math.abs(yearOfBirth)
+    if (dateOfBirth < 0) {
+      dateOfBirth += new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        0
+      ).getDate();
+      monthOfBirth -= 1;
+    }
 
-        })
-    // function isLeapYear(year){
-    //     return (year % 4 === 0 && year %100 !==0 || year % 400 === 0);
-    // }
-    // console.log(isLeapYear(usersYear));
-    // function checkLeapYear(startYear,endYear){
-    //     const leapYears = [];
-    // }+
-})
-app.listen(port,() =>{
-    console.log(`server runing  on port ${port}`);
-})
+    return { dateOfBirth, monthOfBirth, yearOfBirth };
+  };
+  console.log(ageCalculator(usersYear, usersMonth, usersDate));
+  let { dateOfBirth, monthOfBirth, yearOfBirth } = ageCalculator(
+    usersYear,
+    usersMonth,
+    usersDate
+  );
+
+  res.render("index.ejs", {
+    date: Math.abs(dateOfBirth),
+    month: Math.abs(monthOfBirth),
+    year: Math.abs(yearOfBirth),
+  });
+  // function isLeapYear(year){
+  //     return (year % 4 === 0 && year %100 !==0 || year % 400 === 0);
+  // }
+  // console.log(isLeapYear(usersYear));
+  // function checkLeapYear(startYear,endYear){
+  //     const leapYears = [];
+  // }+
+});
+app.listen(port, () => {
+  console.log(`server runing  on port ${port}`);
+});
